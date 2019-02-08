6dbeb91d320ecbe37fca351b703ebae01ff12ab5
Chris Martin <ch.martin@gmail.com>
2019-02-07 21:38:47 -0700

    fill in the pandoc package

--- pandoc-highlighting-extensions/library/Text/Pandoc/Highlighting/Extensions.hs
+++ pandoc-highlighting-extensions/library/Text/Pandoc/Highlighting/Extensions.hs
@@ -1,3 +1,39 @@
 {-# OPTIONS_GHC -Wall #-}
 
-module Text.Pandoc.Highlighting.Extensions where
+module Text.Pandoc.Highlighting.Extensions
+  ( defaultSyntaxMap
+  , extendedSyntaxMap
+  ) where
+
+-- data-default-class
+import Data.Default.Class (def)
+
+-- pandoc
+import qualified Text.Pandoc as Pandoc
+
+-- skylighting-extensions
+import qualified Skylighting.Extensions
+
+-- skylighting-modding
+import Skylighting.Modding (SyntaxMap)
+
+{- |
+
+Pandoc's default syntax highlighting configuration.
+
+-}
+
+defaultSyntaxMap :: SyntaxMap
+defaultSyntaxMap = Pandoc.writerSyntaxMap def
+
+{- |
+
+Pandoc's default syntax highlighting configuration, plus all extensions
+provided by the @skylighting-extensions@ package. Use this for the value
+of 'Pandoc.writerSyntaxMap' to use all of the extensions in your Pandoc
+output.
+
+-}
+
+extendedSyntaxMap :: SyntaxMap
+extendedSyntaxMap = Skylighting.Extensions.applyAll defaultSyntaxMap
---
+++ pandoc-highlighting-extensions/library/Text/Pandoc/Highlighting/Extensions/WriterOptions.hs
@@ -0,0 +1,73 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Text.Pandoc.Highlighting.Extensions.WriterOptions
+  ( applyAllSyntaxExtensions
+  , modifySyntaxMap
+  , modifySyntax
+  , modifyContext
+  ) where
+
+-- pandoc
+import Text.Pandoc (WriterOptions (..))
+
+-- skylighting-core
+import Skylighting.Types (Context, Syntax)
+
+-- skylighting-extensions
+import qualified Skylighting.Extensions
+
+-- skylighting-modding
+import Skylighting.Modding (SyntaxMap)
+import qualified Skylighting.Modding
+
+-- text
+import Data.Text (Text)
+
+{- |
+
+Apply all of the syntax highlighting extensions provided by the
+@skylighting-extensions@ package to Pandoc writer options.
+
+-}
+
+applyAllSyntaxExtensions :: WriterOptions -> WriterOptions
+applyAllSyntaxExtensions = modifySyntaxMap Skylighting.Extensions.applyAll
+
+{- |
+
+Apply a function to the 'SyntaxMap' within Pandoc writer options.
+
+-}
+
+modifySyntaxMap :: (SyntaxMap -> SyntaxMap) -> WriterOptions -> WriterOptions
+modifySyntaxMap f o = o{ writerSyntaxMap = f (writerSyntaxMap o) }
+
+{- |
+
+Modify a particular syntax by name.
+
+-}
+
+modifySyntax
+    :: Text -- ^ Name of the syntax to modify
+    -> (Syntax -> Syntax) -> WriterOptions -> WriterOptions
+
+modifySyntax name =
+    modifySyntaxMap .
+    Skylighting.Modding.modifySyntax name
+
+{- |
+
+Modify a particular syntax context by name.
+
+-}
+
+modifyContext
+    :: Text -- ^ Name of the syntax to modify
+    -> Text -- ^ Name of the context to modify
+    -> (Context -> Context) -> WriterOptions -> WriterOptions
+
+modifyContext syntaxName contextName =
+    modifySyntax syntaxName .
+    Skylighting.Modding.modifySyntaxContexts .
+    Skylighting.Modding.modifyContext contextName
